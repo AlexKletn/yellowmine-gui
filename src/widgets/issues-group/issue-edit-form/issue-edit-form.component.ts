0fa9293ad89fc1050bebdd6dd0da4407230f8486
alex <kletnalex98@gmail.com>
2024-11-08 22:06:55 +0300

    feat: issue edit WIP

---
+++ src/widgets/issues-group/issue-edit-form/issue-edit-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ym-issue-edit-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-edit-form.component.html',
+  styleUrl: './issue-edit-form.component.scss'
+})
+export class IssueEditFormComponent {
+
+}
